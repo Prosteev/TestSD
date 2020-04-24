82ea050e7fbce8b1dbf506eaba575ca389a9b47f
Alex Ivanov <deprosteev@bk.ru>
2020-04-25 00:02:42 +0300

    изменил параметры: время в ФС=const 01.01.2020, увеличил размер стека задаче spiTask, поставил кодовую страницу Win1251

--- FATFS/Target/ffconf.h
+++ FATFS/Target/ffconf.h
@@ -83,7 +83,7 @@
 / Locale and Namespace Configurations
 /-----------------------------------------------------------------------------*/
 
-#define _CODE_PAGE         850
+#define _CODE_PAGE         1251
 /* This option specifies the OEM code page to be used on the target system.
 /  Incorrect setting of the code page can cause a file open failure.
 /
@@ -207,10 +207,10 @@
 / System Configurations
 /----------------------------------------------------------------------------*/
 
-#define _FS_NORTC	0
-#define _NORTC_MON	6
-#define _NORTC_MDAY	4
-#define _NORTC_YEAR	2015
+#define _FS_NORTC	1
+#define _NORTC_MON	1
+#define _NORTC_MDAY	1
+#define _NORTC_YEAR	2020
 /* The _FS_NORTC option switches timestamp feature. If the system does not have
 /  an RTC function or valid timestamp is not needed, set _FS_NORTC to 1 to disable
 /  the timestamp feature. All objects modified by FatFs will have a fixed timestamp
