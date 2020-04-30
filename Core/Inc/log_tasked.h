c3d321aa89e742e3504ad11dbe0e07f1dbff4b27
Alex Ivanov <deprosteev@bk.ru>
2020-04-30 13:22:47 +0300

    добавил структуру сообщения в лог, поля флагов состояния и ошибок, методы псевдо-объекта

--- Core/Inc/log_tasked.h
+++ Core/Inc/log_tasked.h
@@ -1,7 +1,7 @@
 /*
  * log_tasked.h
  *
- * Data & functions for logging to text file on SD-card in RTOS
+ * Pseudo-Object (Data & functions) for logging to text file on SD-card in RTOS
  *
  *  Created on: Apr 27, 2020
  *      Author: iva
@@ -15,7 +15,68 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include "ProjDefs.h"
 
+/* Exported types ------------------------------------------------------------*/
+typedef struct
+{
+	uint32_t id;
+	char mes[LOG_MES_LEN];
+} Log_TMessage;
+
+typedef uint8_t Log_ErrorFlags_t;
+typedef uint8_t Log_StateFlags_t;
+
+/* Exported constants --------------------------------------------------------*/
+/* Log_ErrorFlags_t */
+#define Log_EF_INIT_NO_SD_FATFS	(1<<0) /* SD & FATFS not initialized */
+#define Log_EF_FOPEN_2WRITE		(1<<1) /* log file open to write error */
+#define Log_EF_SET_MAIN_EV_FLAG	(1<<2) /* main_event_flags setting error */
+#define Log_EF_MES_QUEUE_INIT	(1<<3) /* Message Queue initialization error */
+#define Log_EF_MES_QUEUE_PUT	(1<<4) /* Put message into Message Queue error */
+#define Log_EF_MES_QUEUE_GET	(1<<5) /* Get message from Message Queue error */
+#define Log_EF_MES_QUEUE_RESET	(1<<6) /* Message Queue reset error */
+//#define Log_ERR_MEM_POOL_INIT    (0x8) /* Memory Pool initialization error */
+/* Log_StateFlags_t */
+#define Log_SF_OPEN_2WRITE    (1<<1) /* log file opened to write */
+
+/* Exported functions prototypes ---------------------------------------------*/
+
+/*
+ * Initialize Log for writing
+ * Returns TRUE if OK
+ */
+BOOL Log_okInit(void);
+
+/*
+ * Put Message to Log Message Queue (not write to log file!)
+ * Waits if Queue is full
+ * Returns TRUE if OK
+ */
+BOOL Log_okPut(Log_TMessage * Message);
+
+/*
+ * Get next message from Log Message Queue
+ * don't wait if Message Queue is empty
+ * must be called from Log Task
+ * Returns TRUE if next message was get from Queue
+ */
+BOOL Log_okGetFromQueue(Log_TMessage * Message);
+
+/*
+ * Write Message to log file
+ * Returns TRUE if message was written
+ */
+BOOL Log_okWriteToLogFile(Log_TMessage * Message);
+/*
+ * Returns TRUE if Log pseudo-object is ready
+ */
+BOOL Log_isReady(void);
+
+void Log_FileClose(void);
+
+Log_ErrorFlags_t Log_GetErrorFlags(void);
 
 
 #ifdef __cplusplus
--- Core/Src/log_tasked.c
+++ Core/Src/log_tasked.c
@@ -8,3 +8,203 @@
 /* Includes ------------------------------------------------------------------*/
 #include "log_tasked.h"
 
+#include <string.h>
+#include "cmsis_os.h"
+#include "fatfs.h"
+#include "MainEventFlags.h"
+/* Private defines -----------------------------------------------------------*/
+#define QUEUE_MES_COUNT      5
+//#define MP_BLOCK_SIZE      LOG_MES_LEN
+//#define MP_BLOCK_COUNT     5
+/* Private variables ---------------------------------------------------------*/
+static Log_ErrorFlags_t error_flags = 0;
+static Log_StateFlags_t state_flags = 0;
+
+FIL log_file;        /* File object */
+FRESULT fres;   /* FatFs return code */
+
+osMessageQueueId_t MsgQueue = NULL;
+//osMemoryPoolId_t mem_pool; /* Memory Pool for messages to be written to log file */
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private user code ---------------------------------------------------------*/
+
+BOOL Log_okInit(void)
+{
+	uint32_t ev_flag_set_res;
+
+	// check preconditions
+	state_flags=0;
+    if(main_event_flags==NULL)
+    	return FALSE; // flags not exist
+
+    if(osEventFlagsGet(main_event_flags) & MF_LOG_IS_READY)
+    	{return TRUE;} // already initialized
+
+    if( (osEventFlagsGet(main_event_flags) & MF_FATFS_ON_SD_INITED)==0 )
+    {	// SD & FATFS not initialized
+    	error_flags = Log_EF_INIT_NO_SD_FATFS;
+    	return FALSE;
+    }
+
+ 	// basic work
+
+   if( (state_flags & Log_SF_OPEN_2WRITE)==0 )
+   {	// is not opened
+
+	   /* Open a text file */
+	   fres = f_open(&log_file, "a.log", FA_CREATE_ALWAYS | FA_WRITE);
+	   if (fres)
+	   {
+		   error_flags = Log_EF_FOPEN_2WRITE;
+		   return FALSE;
+	   }
+
+	   if(MsgQueue==NULL)
+	   {
+		   MsgQueue = osMessageQueueNew(QUEUE_MES_COUNT, sizeof(Log_TMessage), NULL);
+		   if(MsgQueue == NULL)
+		   {	// Message Queue object is not created
+			   error_flags = Log_EF_MES_QUEUE_INIT;
+			   return FALSE;
+		   }
+	   }
+	   else
+	   {
+		   if(osMessageQueueReset(MsgQueue)!=osOK)
+		   {	// Message Queue object is not reset
+			   error_flags = Log_EF_MES_QUEUE_RESET;
+			   return FALSE;
+		   }
+	   }
+//	   mem_pool = osMemoryPoolNew( MP_BLOCK_COUNT, MP_BLOCK_SIZE, NULL);
+//	   if(mem_pool==0)
+//	   {
+//		   error_flags = Log_ERR_MEM_POOL_INIT;
+//		   return FALSE;
+//	   }
+
+	   state_flags |= Log_SF_OPEN_2WRITE;
+   }
+
+   ev_flag_set_res=
+		   osEventFlagsSet(main_event_flags,MF_LOG_IS_READY);
+   if( (int32_t)ev_flag_set_res < 0 || (ev_flag_set_res & MF_LOG_IS_READY)==0 )
+   {
+	   error_flags |= Log_EF_SET_MAIN_EV_FLAG;
+	   return FALSE;
+   }
+
+   return TRUE;
+}
+
+BOOL Log_okPut(Log_TMessage * Message)
+{
+	osStatus_t status;
+
+	// check preconditions
+	if(Message==NULL)
+		return FALSE;
+	if( !Log_isReady() )
+		return FALSE;
+
+	// basic work
+	status = osMessageQueuePut( MsgQueue, Message, 0, osWaitForever );
+	if(status!=osOK)
+	{
+		state_flags |= Log_EF_MES_QUEUE_PUT;
+		return FALSE;
+	}
+	state_flags &= ~Log_EF_MES_QUEUE_PUT;
+	return TRUE;
+}
+
+BOOL Log_okGetFromQueue(Log_TMessage * Message)
+{
+	osStatus_t status;
+
+	// check preconditions
+	if(Message==NULL)
+		return FALSE;
+	if( !Log_isReady() )
+		return FALSE;
+
+	// basic work
+    status = osMessageQueueGet(MsgQueue, Message, NULL, 0U);   // don't wait for message (try semantics)
+    switch (status)
+    {
+		case osOK:
+			// the message has been retrieved from the queue OR
+			state_flags &= ~Log_EF_MES_QUEUE_GET;
+			return TRUE;
+			break;
+
+		case osErrorResource:
+			//nothing to get from the queue (try semantics)
+			state_flags &= ~Log_EF_MES_QUEUE_GET;
+			return FALSE;
+			break;
+
+		default:
+	    	state_flags |= Log_EF_MES_QUEUE_GET;
+	    	return FALSE;
+			break;
+	}
+}
+
+BOOL Log_okWriteToLogFile(Log_TMessage * Message)
+{
+	UINT len;
+	UINT bw;
+
+	// check preconditions
+	if(Message==NULL)
+		return FALSE;
+	if( !Log_isReady() )
+		return FALSE;
+	if(f_error(&log_file))
+		return FALSE;
+
+	// basic work
+//	if( f_printf(&log_file, "%ld: %s", Message->id,Message->mes) == EOF )
+//	if( f_puts(Message->mes, &log_file) == EOF )
+//		return FALSE;
+	len = strlen(Message->mes);
+	fres = f_write( &log_file, Message->mes, len, &bw );
+	if( fres == FR_OK && bw == len)
+		return TRUE; // write message - ok
+	// write message error
+	if( fres == FR_DISK_ERR || fres == FR_INT_ERR )
+		Log_FileClose();
+	return FALSE;
+}
+
+Log_ErrorFlags_t Log_GetErrorFlags(void)
+{
+	return error_flags;
+}
+
+BOOL Log_isReady(void)
+{
+	if( (state_flags & Log_SF_OPEN_2WRITE)==0 )
+		return FALSE; // log is not opened
+	if(main_event_flags==NULL)
+		return FALSE; // flags not exist
+	if( (osEventFlagsGet(main_event_flags) & MF_LOG_IS_READY)==0 )
+		return FALSE; // log is not initialized
+
+	return TRUE;
+}
+
+void Log_FileClose(void)
+{
+	// check preconditions
+	if( (state_flags & Log_SF_OPEN_2WRITE)==0 )
+		return; // log is not opened
+
+	// basic work
+	fres = f_close(&log_file);
+	state_flags &= ~Log_SF_OPEN_2WRITE;
+	osEventFlagsClear(main_event_flags, MF_LOG_IS_READY);
+}
