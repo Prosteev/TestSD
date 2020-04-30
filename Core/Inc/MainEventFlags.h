7d79adbe17b411076fb2abb218bb87e6e3c18a35
Alex Ivanov <deprosteev@bk.ru>
2020-04-30 13:11:49 +0300

    добавил флаг готовности лога, убрал пока флаги заданий

--- Core/Inc/MainEventFlags.h
+++ Core/Inc/MainEventFlags.h
@@ -15,17 +15,31 @@
 
 /* Exported macro ------------------------------------------------------------*/
 /*
- * project main event flags for spi_event_flags
+ * flags for main_event_flags
  */
-#define MF_FATFS_ON_SD_INITED (0x1) /* FATFS on SD-card initialized successfully */
+#define MF_FATFS_ON_SD_INITED	(0x1) /* FATFS on SD-card initialized successfully */
+#define MF_LOG_IS_READY			(0x2) /* Log object is ready logging */
+
+/*
+ * flags for jobs_event_flags
+ */
+#define JF_WRITE_LOG			(0x1) /* write test message to log */
+
 
 #ifndef I_AM_IN_MAIN_C
-extern
+#define MF_EXTERN extern
+#define MF_ZERO
+#else
+#define MF_EXTERN
+#define MF_ZERO = NULL
 #endif
-osEventFlagsId_t main_event_flags
-#ifdef I_AM_IN_MAIN_C
-= NULL
-#endif
-;
+
+
+MF_EXTERN osEventFlagsId_t main_event_flags MF_ZERO;
+//MF_EXTERN osEventFlagsId_t jobs_event_flags MF_ZERO;
+
+
+#undef MF_EXTERN
+#undef MF_ZERO
 
 #endif /* INC_MAINEVENTFLAGS_H_ */
