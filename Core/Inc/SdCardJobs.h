c15f7dafff4720dc2434e027aeae9c86f0591aa3
Alex Ivanov <deprosteev@bk.ru>
2020-04-30 13:19:25 +0300

    добавил поля флагов состояния и ошибок, функцию определения готовности псевдо-объекта

--- Core/Inc/SdCardJobs.h
+++ Core/Inc/SdCardJobs.h
@@ -15,16 +15,31 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
 #include "ProjDefs.h"
+/* Exported types ------------------------------------------------------------*/
+typedef uint8_t SdFat_ErrorFlags_t;
+typedef uint8_t SdFat_StateFlags_t;
+/* Exported constants --------------------------------------------------------*/
+/* SdFat_ErrorFlags_t */
+#define SdFat_EF_DISK_INIT			(1<<0) /* disk_initialize() error */
+#define SdFat_EF_FMOUNT				(1<<1) /* f_mount error */
+#define SdFat_EF_SET_MAIN_EV_FLAG	(1<<2) /* main_event_flags setting error */
+//#define Log_EF_MES_QUEUE_INIT	(1<<3) /* Message Queue initialization error */
+//#define Log_EF_MES_QUEUE_PUT	(1<<4) /* Put message into Message Queue error */
+//#define Log_EF_MES_QUEUE_GET	(1<<4) /* Get message from Message Queue error */
 
+/* SdF_StateFlags_t */
+#define SdFat_SF_MOUNTED		    (1<<1) /* log file opened to write */
 /* Exported functions prototypes ---------------------------------------------*/
 
 /*
- * Initialize disk & mount FATFS on it
+ * Initialize SD-card & mount FATFS on it
  * Returns TRUE if OK
  */
-BOOL okFatfsOnSd_mount(void);
+BOOL SdFat_okMount(void);
 
+BOOL SdFat_isReady(void);
 
 #ifdef __cplusplus
 }
--- Core/Src/SdCardJobs.c
+++ Core/Src/SdCardJobs.c
@@ -13,9 +13,12 @@
 #include "MainEventFlags.h"
 
 /* Private variables ---------------------------------------------------------*/
+static SdFat_ErrorFlags_t error_flags = 0;
+static SdFat_StateFlags_t state_flags = 0;
+
 FATFS fs;
 
-BOOL okFatfsOnSd_mount(void)
+BOOL SdFat_okMount(void)
 {
 	DSTATUS dstat = RES_OK;
     FRESULT fres;
@@ -25,19 +28,39 @@ BOOL okFatfsOnSd_mount(void)
     	return FALSE; // flags not exist
 
     if(osEventFlagsGet(main_event_flags) & MF_FATFS_ON_SD_INITED)
-    	{return TRUE;} // already initialized
+    {	// already initialized
+    	return TRUE;
+    }
 
 	dstat = disk_initialize(0);
 	if(dstat != RES_OK)
+	{
+		error_flags = SdFat_EF_DISK_INIT;
 		return FALSE;
+	}
 
     fres = f_mount(&fs, "", 1);/* Mount the default drive */
     if(fres!=FR_OK)
+    {
+    	error_flags = SdFat_EF_FMOUNT;
     	return FALSE;
+    }
 
     event_res = osEventFlagsSet(main_event_flags,MF_FATFS_ON_SD_INITED);
     if((int32_t)event_res < 0 || (event_res & MF_FATFS_ON_SD_INITED)==0)
+    {
+    	error_flags = SdFat_EF_SET_MAIN_EV_FLAG;
      	return FALSE;
+    }
 
-   return TRUE;
+    state_flags = SdFat_SF_MOUNTED;
+    error_flags = 0;
+
+    return TRUE;
+}
+
+BOOL SdFat_isReady(void)
+{
+	return ((state_flags & SdFat_SF_MOUNTED) && error_flags == 0)
+			? TRUE : FALSE;
 }
